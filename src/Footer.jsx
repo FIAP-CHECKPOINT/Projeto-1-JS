12248cd43120cac24c8c21a629aa55937d920eaa
gutoFM <gutofisco@gmail.com>
2023-09-11 23:10:52 -0300

    v1

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import { } from 'react'
+
+function Footer(){
+    return(
+        <>
+        <h1>(11)99265-2121 - gutofisco@gmail.com</h1>
+        </>
+    )
+}
+
+export default Footer
---
+++ src/Produtos.jsx
@@ -0,0 +1,21 @@
+import { } from 'react'
+    
+function Produtos(){
+    return(
+        <>
+         <h1>Gaivota</h1>
+         <img src="https://fastly.picsum.photos/id/275/200/300.jpg?hmac=cSMNzJnIBeocVhMi8311gzgp4ZylFL2LlsUWtqobTEs" alt="Gaivota" />
+         
+         <h1>Flores</h1>
+         <img src="https://fastly.picsum.photos/id/517/200/300.jpg?hmac=xpkz9Xo5Fd9o6IumOFou6GwHqhMUTxxhyYfNiawb1Qk" alt="Flores" />
+         
+         <h1>Mar Cinza</h1>
+         <img src="https://fastly.picsum.photos/id/467/200/300.jpg?hmac=sQK5ibuk2pXpFclSCs5TxY7X9hsRsRbb4r5JhWqRErc" alt="Mar Cinza" />
+         
+         <h1>Notebook</h1>
+         <img src="https://fastly.picsum.photos/id/370/200/300.jpg?hmac=7gPWuhI1_LDcGkEssyW-1sPKu9NVl1KUoOs0nH7KXno" alt="Notebook" />
+        </>
+    )
+}
+
+export default Produtos
